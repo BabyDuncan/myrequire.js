cdd345293e66b0238271f5bdb485913eb7d5edda
Guohao Zhao <guohaozhao116008@sohu-inc.com>
2016-06-07 17:50:13 +0800

    rebuild

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('myrequireJs', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngMaterial', 'toastr']);
-
-})();
